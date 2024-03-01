151bead789b56a589927c6dd6518c2086693ed93
ArseneBrosy <arsene.brosy@divtec.ch>
2024-03-01 14:14:01 +0100

    cards

--- js/script.js
+++ js/script.js
@@ -7,16 +7,50 @@ const DAYS = [
   "Vendredi",
   "Samedi"
 ];
-const today = DAYS[new Date().getDay()];
+let today = new Date().getDay();
+
+const menusLayout = document.querySelector("#menus");
 
 let request = new XMLHttpRequest();
 request.open("GET", "https://menu-api.eldora.ch/api/v1/xml/menu/9133", true);
+
+let doc;
+
 request.onreadystatechange = function () {
   if (request.readyState === 4 && request.status === 200)
   {
-    const doc = request.responseXML;
-    console.log(doc.querySelector(`${today} Menu1`));
-    console.log(doc.querySelector(today));
+    doc = request.responseXML;
+    refresh(doc);
   }
 };
 request.send(null);
+
+function refresh(doc) {
+  const libPrix1 = doc.querySelector("LibPrix1").innerHTML;
+  const libPrix2 = doc.querySelector("LibPrix2").innerHTML;
+  document.querySelector("#restaurant-name").innerHTML = doc.querySelector("nomEts").innerHTML;
+  menusLayout.innerHTML = "";
+  for (let menu of doc.querySelector(`${DAYS[today]} Menus`).children) {
+    const htmlCard = `<div class="menu-card">
+        <h1>${menu.querySelector("TitreFr").innerHTML}</h1>
+        <p>${menu.querySelector("CorpsFr").innerHTML.replace(/(\r\n|\r|\n)/g, '<br>')}</p>
+        <div class="menu-footer"><p>${libPrix1} ${menu.querySelector("Prix1").innerHTML}</p>
+        <p>${libPrix2} ${menu.querySelector("Prix2").innerHTML}</p></div></div>`;
+    menusLayout.innerHTML += htmlCard;
+  }
+  document.querySelector("#day-name").innerHTML = DAYS[today];
+}
+
+function previousDay() {
+  if (today > 1) {
+    today--;
+  }
+  refresh(doc);
+}
+
+function nextDay() {
+  if (today < 5) {
+    today++;
+  }
+  refresh(doc);
+}
